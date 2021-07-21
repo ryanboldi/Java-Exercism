13a04aaf817a14930746bd413b295559731977a9
Ryan Boldi <ryan.boldi123@gmail.com>
2021-07-21 10:08:56 -0400

    Update Hamming.java

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,27 +1,24 @@
 public class Hamming {
-    private String left;
-    private String right;
+    private int dist = 0;
 
     public Hamming(String leftStrand, String rightStrand) {
         if(leftStrand.isEmpty() && !rightStrand.isEmpty())   throw new IllegalArgumentException("left strand must not be empty.");
         else if(rightStrand.isEmpty() && !leftStrand.isEmpty())  throw new IllegalArgumentException("right strand must not be empty.");
         else if (leftStrand.length() != rightStrand.length())    throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
         else {
-            left = leftStrand;
-            right = rightStrand;
-        }
+            int tDist = 0;
 
+            for (int i = 0; i < leftStrand.length() ; i++){
+                if (leftStrand.charAt(i) != rightStrand.charAt(i)){
+                    tDist += 1;
+                }
+            }
+
+            dist = tDist;
+        }
     }
 
     public int getHammingDistance() {
-        int dist = 0;
-
-        for (int i = 0; i < left.length() ; i++){
-            if (left.charAt(i) != right.charAt(i)){
-                dist += 1;
-            }
-        }
-
         return dist;
     }
 }
