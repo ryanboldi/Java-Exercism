8f5ea42b223e064bf73cc9f5ca0f702108d810c6
Ryan Boldi <ryan.boldi123@gmail.com>
2021-07-22 08:42:32 -0400

    somehow this works

---
+++ gigasecond/src/main/java/Gigasecond.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+
+public class Gigasecond {
+    TemporalUnit seconds = ChronoUnit.SECONDS;
+    LocalDateTime afterGiga;
+
+    public Gigasecond(LocalDate moment) {
+        LocalDateTime momentTime = moment.atStartOfDay();
+        afterGiga = momentTime.plus(1000000000, seconds);
+    }
+
+    public Gigasecond(LocalDateTime moment) {
+        afterGiga = moment.plus(1000000000, seconds);
+    }
+
+    public LocalDateTime getDateTime() {
+        return afterGiga;
+    }
+}
