97966980823e0d71e68fc329043d5351d19028c3
Ryan Boldi <ryan.boldi123@gmail.com>
2021-07-24 07:50:05 -0400

    Update DifferenceOfSquaresCalculator.java

--- difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,8 +1,6 @@
 import java.lang.Math;
 
 class DifferenceOfSquaresCalculator {
-
-    //URL: https://math.stackexchange.com/q/4063534 (version: 2021-03-16)
     int computeSquareOfSumTo(int input) {
         return (int)Math.pow(input * (input + 1) / 2, 2);        
     }
@@ -14,5 +12,4 @@ class DifferenceOfSquaresCalculator {
     int computeDifferenceOfSquares(int input) {
         return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
     }
-
 }
