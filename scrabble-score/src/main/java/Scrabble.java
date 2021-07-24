368e81a1d25a5a2d48b6d900200b9894b855adc1
Ryan Boldi <ryan.boldi123@gmail.com>
2021-07-24 07:20:41 -0400

    much more concise

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -1,29 +1,20 @@
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.HashMap;
+import java.util.stream.IntStream;
 
 class Scrabble {
-    //i'm sorry in advance.
-    static Map<Character, Integer> values = Map.ofEntries(
-        Map.entry('A', 1), Map.entry('E', 1), Map.entry('I', 1), Map.entry('O', 1), Map.entry('U', 1),
-        Map.entry('L', 1), Map.entry('N', 1), Map.entry('R', 1), Map.entry('S', 1), Map.entry('T', 1),
-        Map.entry('D', 2), Map.entry('G', 2),
-        Map.entry('B', 3), Map.entry('C', 3), Map.entry('M', 3), Map.entry('P', 3),
-        Map.entry('F', 4), Map.entry('H', 4), Map.entry('V', 4), Map.entry('W', 4), Map.entry('Y', 4),
-        Map.entry('K', 5),
-        Map.entry('J', 8), Map.entry('X', 8),
-        Map.entry('Q', 10), Map.entry('Z', 10)
-    );
+    private final static HashMap<Integer, Integer> charToValue = new HashMap<Integer, Integer>(26);
+    private final static String[] letters = {"AEIOULNRST", "DG", "BCMP", "FHVWY", "K", "JX", "QZ"};
+    private final static int[] scores = {1, 2, 3, 4, 5, 8, 10};
 
-    int value = 0;
+    static{
+        IntStream.range(0, scores.length)
+            .forEach(i -> letters[i].chars().forEach(c -> charToValue.put(c, scores[i])));
+    }
+
+    private final int value;
 
     Scrabble(String word) {
-        int sum = 0;
-
-        for (int i = 0; i < word.length(); i++){
-            sum += values.get(Character.toUpperCase(word.charAt(i)));
-        }
-
-        value = sum;
+        value = word.toUpperCase().chars().reduce(0, (tot, newCharNum) -> tot += charToValue.get(newCharNum));
     }
 
     int getScore() {
