16bbf4def8a910fc3d3f4e05cb727f62c80fa611
Ryan Boldi <ryan.boldi123@gmail.com>
2021-08-15 09:37:50 -0400

    handles more than 5 binary digits

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,19 +1,21 @@
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
 
 class HandshakeCalculator {
     List<Signal> calculateHandshake(int number) {
-        //                  [1  2  4  8 16]
-        int[] placeValues = {0, 0, 0, 0, 0};
+        if (number > 127) throw new InvalidParameterException();
+        //                  [1  2  4  8 16 32 64]
+        int[] placeValues = {0, 0, 0, 0, 0, 0, 0};
         int remainder = number;
         ArrayList<Signal> signals = new ArrayList<Signal>();
         
-        for (int i = 4 ; i >= 0; i--){
+        for (int i = 6 ; i >= 0; i--){
             if (remainder >= (Math.pow(2, i))){
                 remainder -= Math.pow(2, i);
                 placeValues[i] = 1;
-                if (i != 4) signals.add(Signal.values()[i]);
+                if (i < 4) signals.add(Signal.values()[i]);
             }
         }
 
