806dd924f6009ee25c93156b3b2c6e6b95297b1a
Ryan Boldi <ryan.boldi123@gmail.com>
2021-08-19 11:56:22 -0400

    passes all tests

--- matrix/src/main/java/Matrix.java
+++ matrix/src/main/java/Matrix.java
@@ -1,15 +1,38 @@
-
 class Matrix {
+    private int[][] matrix;
+    private int width;
+    private int height;
 
     Matrix(String matrixAsString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        //get the length of the first row
+        String lines[] = matrixAsString.split("\\r?\n");
+        String firstLine[] = lines[0].split(" ");
+        width = firstLine.length;
+        height = lines.length;
+        matrix = new int[height][width];
+
+        for (int i = 0; i < height; i++){
+            String line[] = lines[i].split(" ");
+            for (int j = 0; j < width; j++){
+                matrix[i][j] = Integer.parseInt(line[j]);
+            }
+        }
     }
 
     int[] getRow(int rowNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int index = rowNumber -1; // 0 indexing
+
+        return matrix[index];
     }
 
     int[] getColumn(int columnNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int toReturn[] = new int[height];
+        int index = columnNumber - 1;
+
+        for (int i = 0 ; i < height; i++){
+            toReturn[i] = matrix[i][index];
+        }
+
+        return toReturn;
     }
 }
